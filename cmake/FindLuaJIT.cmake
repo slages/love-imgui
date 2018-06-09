4aec73f05c649efb5ba29031be3b8b18bbd2e8f0
Cameron McHenry <camchenry@users.noreply.github.com>
2018-06-09 18:57:34 -0400

    Update lib paths

--- cmake/FindLuaJIT.cmake
+++ cmake/FindLuaJIT.cmake
@@ -5,22 +5,23 @@
 # LUAJIT_LIBRARY
 
 set(LUAJIT_SEARCH_PATHS
-	/usr/local
-	/usr
+    /usr/local
+    /usr
+    c:/usr/local/lib
     c:/usr/local
     c:/usr
-	$ENV{LUAJIT_DIR}
-	)
+    $ENV{LUAJIT_DIR}
+    )
 
 find_path(LUAJIT_INCLUDE_DIR
-	NAMES luajit.h
-	PATH_SUFFIXES include include/luajit-2.0 src
-	PATHS ${LUAJIT_SEARCH_PATHS})
+    NAMES luajit.h
+    PATH_SUFFIXES include include/luajit-2.0 src
+    PATHS ${LUAJIT_SEARCH_PATHS})
 
 find_library(LUAJIT_LIBRARY
-	NAMES luajit-5.1 lua51
-	PATH_SUFFIXES lib
-	PATHS ${LUAJIT_SEARCH_PATHS})
+    NAMES luajit-5.1 lua51
+    PATH_SUFFIXES lib
+    PATHS ${LUAJIT_SEARCH_PATHS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LuaJIT DEFAULT_MSG LUAJIT_LIBRARY LUAJIT_INCLUDE_DIR)
