34bad62f88aff6b5c8944ee4db76cf4ccdc623f8
slages <serge.lages@gmail.com>
2019-03-23 14:09:51 +0100

    Update wrap_imgui_impl.cpp
    Added support for strndup

--- src/wrap_imgui_impl.cpp
+++ src/wrap_imgui_impl.cpp
@@ -26,6 +26,21 @@
 #include <vector>
 #include <cstring>
 
+#if defined(_WIN32) || defined(_WIN64)
+
+#include <math.h>
+
+// strndup() is not available on Windows
+char *strndup( const char *s1, size_t n)
+{
+    char *copy= (char*)malloc( n+1 );
+    memcpy( copy, s1, n );
+    copy[n] = 0;
+    return copy;
+};
+
+#endif
+
 /*
 ** Love implentation functions
 */
